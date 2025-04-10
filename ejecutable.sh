57acb8990b1ec63d4afdc7c1ee0e34fc13bc78c3
AndresGarcia15 <teherana@utb.edu.co>
2025-04-10 00:16:00 -0500

    actualizacion ejecutable

--- ejecutable.sh
+++ ejecutable.sh
@@ -3,22 +3,28 @@ set -e  # Detener el script si ocurre un error
 
 # Configurar la localización
 export LANG=en_US.UTF-8
-# sudo -i
 
 # Navegar al directorio del proyecto
-cd /home/ubuntu/Lumaweb/Datos_Luma|| { echo "Error: No se pudo acceder a /root/LumaExecutable"; exit 1; }
+cd /home/pipe15200/LumaExecutable || { echo "Error: No se pudo acceder a /home/pipe15200/LumaExecutable"; exit 1; }
 
 # Verificar si el entorno virtual existe antes de activarlo
 if [ -d ".venv" ]; then
-    source /home/ubuntu/Lumaweb/Datos_Luma/.venv/bin/activate
+    echo "Activando entorno virtual existente..."
+    source .venv/bin/activate
 else
-    echo "Error: No se encontró el entorno virtual .venv. Creándolo..."
-    sudo python3 -m venv .venv
-    source /home/ubuntu/Lumaweb/Datos_Luma/.venv/bin/activate
+    echo "No se encontró el entorno virtual .venv. Creándolo..."
+    python3 -m venv .venv
+    source .venv/bin/activate
 fi
 
-# Instalar los requisitos
-pip install -r requirements.txt
+# Asegurar que pip esté actualizado
+pip install --upgrade pip
 
+# Instalar los requisitos
+if ! pip install -r requirements.txt; then
+    echo "❌ Error al instalar los paquetes. Revisa los permisos o el contenido del archivo requirements.txt."
+    exit 1
+fi
+sleep 1
 # Ejecutar el script en Python
-exec python3 geturls.py  # Usa exec para reemplazar el proceso actual
+exec python3 geturls.py
